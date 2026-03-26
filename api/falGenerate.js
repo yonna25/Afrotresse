2b318f782afa63871ad866167ce14aad8b500c3d
yonna25 <yonnadigit@gmail.com>
2026-03-26 11:44:13 +0100

    Update falGenerate.js

--- api/falGenerate.js
+++ api/falGenerate.js
@@ -3,7 +3,7 @@
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(405).json({ error: "Method not allowed" }); 
   }
 
   try {
